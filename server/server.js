159c54baa05a6a6a6e7fe56d054d6f7d765eadc5
Dharmesh Yadav <166201809+Dharmesh-yadavo@users.noreply.github.com>
2025-12-31 14:05:14 +0530

    users model created

--- server/server.js
+++ server/server.js
@@ -7,11 +7,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
 
-cors({
-  origin: process.env.CLIENT_URL,
-  methods: ["GET", "POST", "DELETE", "PUT"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-});
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 app.use(express.json());
 
