618c3002e8d981b468ee56b94fa5329190d6db18
Dharmesh Yadav <166201809+Dharmesh-yadavo@users.noreply.github.com>
2026-01-02 00:57:48 +0530

    instructor layout ui created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import { AuthPage } from "./pages/auth/index.jsx";
 import { RouteGuard } from "./components/route-guard/index.jsx";
 import { useContext } from "react";
 import { AuthContext } from "./context/auth-context/index.jsx";
-import { InstructorDashboard } from "./pages/instructor/index.jsx";
+import { InstructorDashboardPage } from "./pages/instructor/index.jsx";
 import { StudentViewCommonLayout } from "./components/student-view/common-layout.jsx";
 import { StudentHomePage } from "./pages/student/index.jsx";
 import { NotFoundPage } from "./pages/not-found/index.jsx";
@@ -26,7 +26,7 @@ function App() {
       path: "/instructor",
       element: (
         <RouteGuard
-          element={<InstructorDashboard />}
+          element={<InstructorDashboardPage />}
           authenticated={auth?.authenticated}
           user={auth?.user}
         />
---
+++ client/src/components/instructor-view/courses/index.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const InstructorCourses = () => {
+  return <div>InstructorCourses</div>;
+};
