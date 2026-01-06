cb792e58b2178e55fa80eb95518651ac403b21f6
Dharmesh Yadav <166201809+Dharmesh-yadavo@users.noreply.github.com>
2026-01-06 17:22:31 +0530

    Instructor_Course page created

--- client/src/components/instructor-view/courses/index.jsx
+++ client/src/components/instructor-view/courses/index.jsx
@@ -1,5 +1,55 @@
-import React from "react";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Delete, Edit } from "lucide-react";
 
 export const InstructorCourses = () => {
-  return <div>InstructorCourses</div>;
+  return (
+    <>
+      <Card>
+        <CardHeader className="flex flex-row justify-between items-center">
+          <CardTitle className="text-2xl font-extrabold">All Courses</CardTitle>
+          <Button className="p-6">Create New Courses</Button>
+        </CardHeader>
+        <CardContent>
+          <div className="">
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead>Course</TableHead>
+                  <TableHead>Students</TableHead>
+                  <TableHead>Revenue</TableHead>
+                  <TableHead className="text-right">Actions</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                <TableRow>
+                  <TableCell className="font-medium">
+                    React Js Full Course 2025
+                  </TableCell>
+                  <TableCell>100</TableCell>
+                  <TableCell>$5000</TableCell>
+                  <TableCell className="text-right">
+                    <Button variant="ghost" size="sm">
+                      <Edit className="h-6 w-6" />
+                    </Button>
+                    <Button variant="ghost" size="sm">
+                      <Delete className="h-6 w-6" />
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </div>
+        </CardContent>
+      </Card>
+    </>
+  );
 };
