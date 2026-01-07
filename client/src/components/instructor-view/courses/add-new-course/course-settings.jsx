264ceefa2a895d316c3a9223b68e83c62cb6c5a8
Dharmesh Yadav <166201809+Dharmesh-yadavo@users.noreply.github.com>
2026-01-08 00:14:32 +0530

    mediaUploadService in media-settings

--- client/src/components/instructor-view/courses/add-new-course/course-settings.jsx
+++ client/src/components/instructor-view/courses/add-new-course/course-settings.jsx
@@ -1,19 +1,55 @@
+/* eslint-disable no-unused-vars */
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { InstructorContext } from "@/context/instructor-context";
+import { mediaUploadService } from "@/servises";
+import { useContext } from "react";
 
 const CourseSettings = () => {
+  const { courseLandingFormData, setCourseLandingFormData } =
+    useContext(InstructorContext);
+
+  const handleImageUploadChange = async (event) => {
+    const selectedImage = event.target.files[0];
+
+    if (selectedImage) {
+      const imageFormData = new FormData();
+      imageFormData.append("file", selectedImage);
+
+      try {
+        const response = await mediaUploadService(imageFormData);
+        if (response.success) {
+          setCourseLandingFormData({
+            ...courseLandingFormData,
+            image: response.data.url,
+          });
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
   return (
     <Card>
       <CardHeader>
         <CardTitle>Course Settings</CardTitle>
       </CardHeader>
       <CardContent>
-        <div className="flex flex-col gap-3">
-          <Label>Upload Course Image</Label>
-          <Input type="file" accept="image/*" />
-        </div>
+        {courseLandingFormData?.image ? (
+          <img src={courseLandingFormData.image} />
+        ) : (
+          <div className="flex flex-col gap-3">
+            <Label>Upload Course Image</Label>
+            <Input
+              onChange={handleImageUploadChange}
+              type="file"
+              accept="image/*"
+            />
+          </div>
+        )}
       </CardContent>
     </Card>
   );
