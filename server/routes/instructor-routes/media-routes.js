35c642fe7e26e6b0b69197e510dfd7c9d1edeb03
Dharmesh Yadav <166201809+Dharmesh-yadavo@users.noreply.github.com>
2026-01-07 22:23:36 +0530

    backend functionality for uploads & delete done using cloudinary

---
+++ server/routes/instructor-routes/media-routes.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const multer = require("multer");
+const {
+  uploadMediaToCloudinary,
+  deleteMediaFromCloudinary,
+} = require("../../helpers/cloudinary.js");
+
+const router = express.Router();
+
+const upload = multer({ dest: "uploads/" });
+
+router.post("/uploads", async (req, res) => {
+  try {
+    console.log(req);
+    const result = await uploadMediaToCloudinary(req.file.path);
+    res.status(200).json({
+      success: true,
+      data: result,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: "Error uploading file" });
+  }
+});
+
+router.delete("/delete/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    if (!id) {
+      return res.status(400).json({
+        success: false,
+        message: "Assest Id is required",
+      });
+    }
+
+    await deleteMediaFromCloudinary(id);
+
+    res.status(200).json({
+      success: true,
+      message: "Assest deleted successfully from cloudinary",
+    });
+  } catch (e) {
+    console.log(e);
+
+    res.status(500).json({ success: false, message: "Error deleting file" });
+  }
+});
+
+module.exports = router;
